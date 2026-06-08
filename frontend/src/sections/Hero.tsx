2ae8cb8d4d68692c38cb74009a81a0ed2ba75e83
MarcinMarekRuman <marcinruman123@gmail.com>
2026-06-08 20:28:08 +0200

    Avatar Background

--- frontend/src/sections/Hero.tsx
+++ frontend/src/sections/Hero.tsx
@@ -1,16 +1,25 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import '../styles/HeroBg.css'
 
 import Avatar from "../components/Avatar";
 export default function Hero() {
     return (
         <section className="avatarSection">
+            <div className="orb orb-1" />
+            <div className="orb orb-2" />
+            <div className="orb orb-3" />
+            <div className="grid-overlay" />
+            <div className="glow-ring" />
             <div className="canvasContainer">
                 <Canvas camera={{ position: [0, 1, 8], fov: 45 }}>
                     <ambientLight intensity={1} />
                     <directionalLight position={[5, 5, 5]} />
                     <Avatar />
-                    <OrbitControls enableZoom={false} />
+                    <OrbitControls enableZoom={false}
+                        autoRotate={true}
+                        autoRotateSpeed={-2}
+                    />
                 </Canvas>
             </div>
         </section>
